483295831b83523be4ad48decd6d84f11f07da5c
nvmarzakov <nikolay.marzakov@gmail.com>
2024-02-13 10:43:34 +0200

    added accounts paths and views

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -1,3 +1,15 @@
-urlpatterns = (
+import profile
 
+from django.urls import path, include
+
+from petstagram.accounts.views import login_account, register_account, details_account, edit_account, delete_account
+
+urlpatterns = (
+    path('login/', login_account, name='login-page'),
+    path('register/', register_account, name='register-page'),
+    path('profile/<int:pk>/', include([
+        path('', details_account, name='details-page'),
+        path('edit/', edit_account, name='edit-page'),
+        path('delete/', delete_account, name='delete-page'),
+    ]))
 )
--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_account(request):
+    return render(request, 'accounts/login-page.html')
+
+
+def register_account(request):
+    return render(request, 'accounts/register-page.html')
+
+
+def details_account(request, pk):
+    return render(request, 'accounts/profile-details-page.html')
+
+
+def edit_account(request, pk):
+    return render(request, 'accounts/profile-edit-page.html')
+
+
+def delete_account(request, pk):
+    return render(request, 'accounts/profile-delete-page.html')
