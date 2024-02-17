64bca1e54cefd6825044360d70f0d660bcfd36f9
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2024-02-17 16:40:26 +0200

    add static files, images and urls in templates

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -6,6 +6,6 @@ urlpatterns = (
     path('add/', views.add_photo, name="add-photo"),
     path('<int:pk>/', include([
         path('', views.details_photo, name="details-photo"),
-        path('edit', views.edit_photo, name="edit-photo"),
+        path('edit/', views.edit_photo, name="edit-photo"),
     ]))
 )
