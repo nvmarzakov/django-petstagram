9306ee874f7d1695035e936e97d102f9e607bf67
nvmarzakov <nikolay.marzakov@gmail.com>
2024-03-19 09:23:11 +0200

    Create Model Pet

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Pet(models.Model):
+    MAX_NAME = 30
+
+    name = models.CharField(
+        max_length=MAX_NAME,
+        blank=False,
+        null=False,
+    )
+
+    personal_photo = models.URLField(
+        blank=False,
+        null=False,
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=False,
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
